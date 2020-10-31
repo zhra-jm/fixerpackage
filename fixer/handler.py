f3acc09976a16574770e72c49058b16476691d31
zahra-jm <zahrajam1999@gmail.com>
2020-11-01 01:16:26 +0330

    first commit

---
+++ fixer/handler.py
@@ -0,0 +1,2 @@
+def get_rates():
+    print('hi')
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='fixer',
+      version='1.0',
+      packages=['fixer'],
+      url='#',
+      zip_safe=False)
